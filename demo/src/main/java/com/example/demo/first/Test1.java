b1cb6bdb5942aa3b1f5d1e2dfbf6a82c15946692
chenjian <chenjian@suneee.com>
2018-07-11 10:52:39 +0800

    新建线程项目

---
+++ demo/src/main/java/com/example/demo/first/Test1.java
@@ -0,0 +1,18 @@
+package com.example.demo.first;
+
+/**
+ * FileName: Test1
+ * Author:   SunEee
+ * Date:     2018/7/11 10:21
+ * Description:
+ */
+
+public class Test1{
+
+
+
+
+    public void test1() {
+
+    }
+}
