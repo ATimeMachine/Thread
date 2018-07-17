18a5771768a6f2949294718a4199d994ffbe6a57
chenjian <chenjian@suneee.com>
2018-07-17 16:35:44 +0800

    第2章：synchronized

---
+++ chapter-two/src/main/java/com/example/thread/chaptertwo/ChapterTwoApplication.java
@@ -0,0 +1,12 @@
+package com.example.thread.chaptertwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapterTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapterTwoApplication.class, args);
+	}
+}
