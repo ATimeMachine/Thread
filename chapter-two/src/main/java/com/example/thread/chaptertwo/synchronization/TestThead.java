a08a9f3f86c81df593a2e01b1a7f8c3f695a6cfa
chenjian <chenjian@suneee.com>
2018-07-18 11:53:29 +0800

    第2章：synchronized

--- chapter-two/src/main/java/com/example/thread/chaptertwo/synchronization/TestThead.java
+++ chapter-two/src/main/java/com/example/thread/chaptertwo/synchronization/TestThead.java
@@ -14,8 +14,8 @@ public class TestThead {
         MyTheadB b = new MyTheadB(publicParameter);
         b.setName("B");
         a.start();   //开启异步执行
-        publicParameter.getValue(); //虽然获取值加了synchronized，但是获取值执行快，所以值为空
-        Thread.sleep(50);//确定A线程先执行
+        publicParameter.getValue(); //虽然A线程已经开始，即使取值加了synchronized，但是获取值执行快，所以值为空
+        Thread.sleep(50);//确定A线程先执行赋值
         b.start(); //B线程获取值，但是由于加了synchronized，所以不是空
         publicParameter.getValue();//由于有synchronized，但是A线程已经执行赋值，获取值要等到A线程释放锁才行
 
