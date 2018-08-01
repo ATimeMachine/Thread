36e7bc752aaa0e8b24fe78766f7faf0a2abe56f0
chenjian <chenjian@suneee.com>
2018-08-01 12:00:56 +0800

    信号量的作用

--- e-semaphore/src/main/java/com/example/thread/esemaphore/parking/semaphore/TestSemaphore.java
+++ e-semaphore/src/main/java/com/example/thread/esemaphore/parking/semaphore/TestSemaphore.java
@@ -9,6 +9,10 @@ import java.util.concurrent.Semaphore;
  * Author:   SunEee
  * Date:     2018/8/1 11:44
  * Description:
+ * 在java中，使用了synchronized关键字和Lock锁实现了资源的并发访问控制，在同一时间只允许唯一了线程进入临界区访问资源(读锁除外)，
+ * 这样子控制的主要目的是为了解决多个线程并发同一资源造成的数据不一致的问题。在另外一种场景下，一个资源有多个副本可供同
+ * 时使用，比如打印机房有多个打印机、厕所有多个坑可供同时使用，这种情况下，Java提供了另外的并发访问控制--资源的多副本的
+ * 并发访问控制，今天学习的信号量Semaphore即是其中的一种。
  */
 public class TestSemaphore {
     public static void main(String[] args) throws InterruptedException {
